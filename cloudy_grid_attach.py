431f5e7aba79590c9dec5f461189fdb96036e3d3
Britton Smith <brittonsmith@gmail.com>
2009-06-01 12:03:03 -0600

    Added grid graft routines and attribute routines.

---
+++ cloudy_grid_attach.py
@@ -0,0 +1,103 @@
+import h5py
+import numpy as na
+from hdf5_attributes import *
+
+def add_grid_dimension(grid,dimension,size):
+    "Add a dimension to the grid with duplicate data."
+
+    oldShape = grid.shape
+    newShape = list(oldShape)
+
+    newShape.reverse()
+    newShape.append(size)
+    newShape.reverse()
+
+    newGrid = na.zeros(newShape,dtype=grid.dtype)
+    newGrid[:] = grid
+
+    if dimension > 0:
+        newGrid = na.rollaxis(newGrid,0,dimension+1)
+
+    return newGrid
+
+def graft_grid(input_lt,input_ht,outputFile,
+               data_fields=['Heating','Cooling','MMW'],
+               extra_field="metal free electron fraction"):
+    """
+    Attach low temperature and high temperature cooling grids.  The high 
+    temperature grid will have one less dimension than the low temperature 
+    grid, so duplicate data will be made in one dimension.  Fixed electron 
+    fractions are used to make the low temperature data, but not the high 
+    temperature data, as this causes errors in Cloudy.
+    """
+
+    # Open low temperature data and find dimension of the field not in the 
+    # high temperature data.
+
+    extra_dim = -1
+    data_lt = {}
+
+    input = h5py.File(input_lt,'r')
+    print "Reading file: %s." % input_lt
+    for dataset in input.listnames():
+        data_lt[dataset] = input[dataset].value
+    attributes_lt = get_attributes(input)
+    input.close()
+
+    for dim in range(len(data_lt[data_fields[0]].shape)-1):
+        name = "Parameter%d" % (dim+1)
+        if attributes_lt[name]["Name"]['value'] == extra_field:
+            extra_dim = dim
+    if extra_dim < 0:
+        print "Field, %s, not found in %s." % (extra_field,input_lt)
+        return None
+
+    # Open high temperature data and create duplicate data in 
+    # electron fraction dimension.
+
+    data_ht = {}
+    input = h5py.File(input_ht,'r')
+    print "Reading file: %s." % input_ht
+    for dataset in input.listnames():
+        data_ht[dataset] = input[dataset].value
+    attributes_ht = get_attributes(input)
+    input.close()
+
+    print "Combining datasets."
+
+    data_ht_new = {}
+    for dataset in data_fields:
+        data_ht_new[dataset] = add_grid_dimension(data_ht[dataset],extra_dim,
+                                                  (data_lt[dataset].shape)[extra_dim])
+
+    # Remove redundant temperature point.
+    redundant_point = False
+    if data_lt['Temperature'][-1] == data_ht['Temperature'][0]:
+        redundant_point = True
+
+    if redundant_point:
+        data_lt['Temperature'] = na.concatenate((data_lt['Temperature'],data_ht['Temperature'][1:]))
+    else:
+        data_lt['Temperature'] = na.concatenate((data_lt['Temperature'],data_ht['Temperature']))
+
+    del data_ht
+
+    # Concatenate datasets.
+    for dataset in data_fields:
+        if redundant_point:
+            data_ht_copy = data_ht_new[dataset]
+            data_ht_copy = na.rollaxis(data_ht_copy,(len(data_ht_copy.shape)-1),0)
+            data_ht_copy = data_ht_copy[1:]
+            data_ht_copy = na.rollaxis(data_ht_copy,0,len(data_ht_copy.shape))
+        else:
+            data_ht_copy = data_ht_new[dataset]
+
+        data_lt[dataset] = na.concatenate((data_lt[dataset],data_ht_copy),axis=-1)
+
+    # Write new dataset.
+    print "Writing file: %s." % outputFile
+    output = h5py.File(outputFile,'w')
+    for dataset in data_lt.keys():
+        output.create_dataset(dataset,data=data_lt[dataset])
+    write_attributes(output,attributes_lt)
+    output.close()
---
+++ hdf5_attributes.py
@@ -0,0 +1,48 @@
+import h5py
+import numpy as na
+
+dataTypes = {"<type 'float'>":'>f4',"<type 'int'>":'>i4'}
+
+def get_attributes(file):
+    "Get all dataset attributes and their datatypes."
+
+    attributes = {}
+
+    closeFile = False
+
+    if type(file) == h5py.highlevel.File:
+        input = file
+    else:
+        input = h5py.File(file,'r')
+        closeFile = True
+
+    # Get all dataset attributes.
+    for dataset in input.listnames():
+        attributes[dataset] = {}
+        for attribute in input[dataset].attrs.listnames():
+            attributes[dataset][attribute] = {}
+            if type(input[dataset].attrs[attribute]) == na.ndarray:
+                attributes[dataset][attribute]['dtype'] = input[dataset].attrs[attribute].dtype
+            else:
+                thisType = type(input[dataset].attrs[attribute])
+                if dataTypes.has_key("%s" % thisType):
+                    attributes[dataset][attribute]['dtype'] = dataTypes["%s" % thisType]
+                else:
+                    attributes[dataset][attribute]['dtype'] = thisType
+            attributes[dataset][attribute]['value'] = input[dataset].attrs[attribute]
+
+    if closeFile:
+        input.close()
+
+    return attributes
+
+def write_attributes(output,attributes):
+    "Write out all dataset attributes."
+
+    for dataset in attributes.keys():
+        for attribute in attributes[dataset].keys():
+            if type(attributes[dataset][attribute]['value']) == na.ndarray:
+                output[dataset].attrs[attribute] = attributes[dataset][attribute]['value']
+            else:
+                output[dataset].attrs[attribute] = na.array(attributes[dataset][attribute]['value'],
+                                                            dtype=attributes[dataset][attribute]['dtype'])
