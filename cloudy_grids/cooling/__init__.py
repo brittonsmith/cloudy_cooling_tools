78be367625026dbc9493ad5358f162cfc3efda3f
Britton Smith <brittonsmith@gmail.com>
2021-01-22 16:55:13 +0000

    Combine cooling functions into one file.

--- cloudy_grids/cooling/__init__.py
+++ cloudy_grids/cooling/__init__.py
@@ -1,3 +1,1 @@
-from .cloudy_ascii_hdf5 import convert_cooling_tables
-from .cloudy_grid_attach import graft_cooling_tables
-from .zero_dataset import zero_dataset
+from .cloudy_ascii_hdf5 import convert_cooling_tables, graft_cooling_tables, zero_dataset
--- cloudy_grids/cooling/cloudy_ascii_hdf5.py
+++ cloudy_grids/cooling/cloudy_ascii_hdf5.py
@@ -3,7 +3,10 @@ import numpy as np
 import re
 import copy
 
-from cloudy_grids.utilities import get_grid_indices
+from cloudy_grids.utilities import \
+     get_grid_indices, \
+     get_attributes, \
+     write_attributes
 
 floatType = '>f8'
 intType = '>i8'
@@ -135,3 +138,135 @@ def loadMap(mapFile,gridDimension,indices,gridData):
     gridData[1][tuple(indices)][:] = h
     gridData[2][tuple(indices)][:] = c
     gridData[3][tuple(indices)][:] = m
+
+def graft_cooling_tables(input_lt,input_ht,outputFile,
+                         data_fields=['Heating','Cooling','MMW'],
+                         extra_field="metal free electron fraction"):
+    """
+    Attach low temperature and high temperature cooling grids.  The high 
+    temperature grid will have one less dimension than the low temperature 
+    grid, so duplicate data will be made in one dimension.  Fixed electron 
+    fractions are used to make the low temperature data, but not the high 
+    temperature data, as this causes errors in Cloudy.
+    """
+
+    # Open low temperature data and find dimension of the field not in the 
+    # high temperature data.
+
+    extra_dim = -1
+    data_lt = {}
+
+    input = h5py.File(input_lt,'r')
+    print ("Reading file: %s." % input_lt)
+    for dataset in input.listnames():
+        data_lt[dataset] = input[dataset].value
+    attributes_lt = get_attributes(input)
+    input.close()
+
+    for dim in range(len(data_lt[data_fields[0]].shape)-1):
+        name = "Parameter%d" % (dim+1)
+        if attributes_lt[name]["Name"]['value'] == extra_field:
+            extra_dim = dim
+    if extra_dim < 0:
+        print ("Field, %s, not found in %s." % (extra_field,input_lt))
+        return None
+
+    # Open high temperature data and create duplicate data in 
+    # electron fraction dimension.
+
+    data_ht = {}
+    input = h5py.File(input_ht,'r')
+    print ("Reading file: %s." % input_ht)
+    for dataset in input.listnames():
+        data_ht[dataset] = input[dataset].value
+    attributes_ht = get_attributes(input)
+    input.close()
+
+    print ("Combining datasets.")
+
+    data_ht_new = {}
+    for dataset in data_fields:
+        data_ht_new[dataset] = add_grid_dimension(data_ht[dataset],extra_dim,
+                                                  (data_lt[dataset].shape)[extra_dim])
+
+    # Remove redundant temperature point.
+    redundant_point = False
+    if data_lt['Temperature'][-1] == data_ht['Temperature'][0]:
+        redundant_point = True
+
+    if redundant_point:
+        data_lt['Temperature'] = np.concatenate((data_lt['Temperature'],data_ht['Temperature'][1:]))
+    else:
+        data_lt['Temperature'] = np.concatenate((data_lt['Temperature'],data_ht['Temperature']))
+
+    attributes_lt['Temperature']["Dimension"]['value'][0] = data_lt['Temperature'].size
+    del data_ht
+
+    # Change dimension attribute.
+    for dataset in data_fields:
+        attributes_lt[dataset]["Dimension"]['value'][-1] = data_lt['Temperature'].size
+
+    # Concatenate datasets.
+    for dataset in data_fields:
+        if redundant_point:
+            data_ht_copy = data_ht_new[dataset]
+            data_ht_copy = np.rollaxis(data_ht_copy,(len(data_ht_copy.shape)-1),0)
+            data_ht_copy = data_ht_copy[1:]
+            data_ht_copy = np.rollaxis(data_ht_copy,0,len(data_ht_copy.shape))
+        else:
+            data_ht_copy = data_ht_new[dataset]
+
+        data_lt[dataset] = np.concatenate((data_lt[dataset],data_ht_copy),axis=-1)
+
+    # Write new dataset.
+    print ("Writing file: %s." % outputFile)
+    output = h5py.File(outputFile,'w')
+    for dataset in data_lt.keys():
+        output.create_dataset(dataset,data=data_lt[dataset])
+    write_attributes(output,attributes_lt)
+    output.close()
+
+def add_grid_dimension(grid,dimension,size):
+    "Add a dimension to the grid with duplicate data."
+
+    oldShape = grid.shape
+    newShape = list(oldShape)
+
+    newShape.reverse()
+    newShape.append(size)
+    newShape.reverse()
+
+    newGrid = np.zeros(newShape,dtype=grid.dtype)
+    newGrid[:] = grid
+
+    if dimension > 0:
+        newGrid = np.rollaxis(newGrid,0,dimension+1)
+
+    return newGrid
+
+def zero_dataset(input_file,data_fields=['Heating','MMW'],output_file=None):
+    "Set all values in a dataset to zero."
+
+    if output_file is None:
+        output_file = input_file
+
+    data = {}
+
+    print ("Reading file: %s." % input_file)
+    input = h5py.File(input_file,'r')
+    for dataset in input.keys():
+        data[dataset] = input[dataset].value
+    attributes = get_attributes(input)
+    input.close()
+
+    for dataset in data_fields:
+        print ("Setting %s to zero." % dataset)
+        data[dataset][:] = 0.0
+
+
+    print ("Writing file: %s." % output_file)
+    output = h5py.File(output_file,'w')
+    for dataset in data.keys():
+        output.create_dataset(dataset,data=data[dataset])
+    write_attributes(output,attributes)
+    output.close()
